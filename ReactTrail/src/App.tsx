740311886cdce44e90f4b7b4dd56e4d5d1ad982a
Romain Thibaud <romainthibaud45@gmail.com>
2023-01-09 11:50:41 +0100

    remove(css): App.css

--- ReactTrail/src/App.tsx
+++ ReactTrail/src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import { Outlet } from "react-router-dom"
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
