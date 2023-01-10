19cc8a3e78feec9cd32f77d90907a889daabbfb5
TDpy <toto.dupuy6558@gmail.com>
2023-01-10 11:16:36 +0100

    récupération liste courses via API

--- ReactTrail/src/Components/trailList/index.tsx
+++ ReactTrail/src/Components/trailList/index.tsx
@@ -1,239 +1,49 @@
-import './index.css'
+import "./index.css";
 import { list } from "postcss";
+import { useEffect, useState } from "react";
 
 const List = () => {
-    return (
-        <div className='list'>
-            <div className="list-container">
-                <a href={"/details"}>
-                    <div className="card">
-                        <div className="card-header">
-                            <img src="https://www.belambra.fr/les-echappees/wp-inside/uploads/2019/12/perdu-randonnee-reflexes.jpg" alt="rover" />
-                        </div>
-                        <div className="card-body">
-                            <h4>
-                                Dimanche 25 Février
-                            </h4>
-                            <h3>
-                                Trail du vieux Lac
-                            </h3>
-                            <p>
-                                Course à pied en nature, sur sentiers et chemins de montagne. Dépassez vos limites et admirez la beauté de la nature.
-                            </p>
-                            <div className="flex">
-                                <div className="user">
-                                    <img src="https://img1.freepng.fr/20180714/ggq/kisspng-user-profile-computer-icons-login-clip-art-profile-picture-icon-5b49de2f1ef441.4301202215315676631268.jpg" alt="user" />
-                                    <div className="user-info">
-                                        <h5>Bastien Oswald</h5>
-                                        <small>2h ago</small>
-                                    </div>
-                                </div>
-                                <div className='distance'>25 km</div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
+  const [courses, setCourses] = useState([]);
 
-                <a href={"/details"}>
-                    <div className="card">
-                        <div className="card-header">
-                            <img src="https://www.belambra.fr/les-echappees/wp-inside/uploads/2019/12/perdu-randonnee-reflexes.jpg" alt="rover" />
-                        </div>
-                        <div className="card-body">
-                            <h4>
-                                Dimanche 25 Février
-                            </h4>
-                            <h3>
-                                Trail du vieux Lac
-                            </h3>
-                            <p>
-                                Course à pied en nature, sur sentiers et chemins de montagne. Dépassez vos limites et admirez la beauté de la nature.
-                            </p>
-                            <div className="flex">
-                                <div className="user">
-                                    <img src="https://img1.freepng.fr/20180714/ggq/kisspng-user-profile-computer-icons-login-clip-art-profile-picture-icon-5b49de2f1ef441.4301202215315676631268.jpg" alt="user" />
-                                    <div className="user-info">
-                                        <h5>Bastien Oswald</h5>
-                                        <small>2h ago</small>
-                                    </div>
-                                </div>
-                                <div className='distance'>25 km</div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <a href={"/details"}>
-                    <div className="card">
-                        <div className="card-header">
-                            <img src="https://www.belambra.fr/les-echappees/wp-inside/uploads/2019/12/perdu-randonnee-reflexes.jpg" alt="rover" />
-                        </div>
-                        <div className="card-body">
-                            <h4>
-                                Dimanche 25 Février
-                            </h4>
-                            <h3>
-                                Trail du vieux Lac
-                            </h3>
-                            <p>
-                                Course à pied en nature, sur sentiers et chemins de montagne. Dépassez vos limites et admirez la beauté de la nature.
-                            </p>
-                            <div className="flex">
-                                <div className="user">
-                                    <img src="https://img1.freepng.fr/20180714/ggq/kisspng-user-profile-computer-icons-login-clip-art-profile-picture-icon-5b49de2f1ef441.4301202215315676631268.jpg" alt="user" />
-                                    <div className="user-info">
-                                        <h5>Bastien Oswald</h5>
-                                        <small>2h ago</small>
-                                    </div>
-                                </div>
-                                <div className='distance'>25 km</div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <a href={"/details"}>
-                    <div className="card">
-                        <div className="card-header">
-                            <img src="https://www.belambra.fr/les-echappees/wp-inside/uploads/2019/12/perdu-randonnee-reflexes.jpg" alt="rover" />
-                        </div>
-                        <div className="card-body">
-                            <h4>
-                                Dimanche 25 Février
-                            </h4>
-                            <h3>
-                                Trail du vieux Lac
-                            </h3>
-                            <p>
-                                Course à pied en nature, sur sentiers et chemins de montagne. Dépassez vos limites et admirez la beauté de la nature.
-                            </p>
-                            <div className="flex">
-                                <div className="user">
-                                    <img src="https://img1.freepng.fr/20180714/ggq/kisspng-user-profile-computer-icons-login-clip-art-profile-picture-icon-5b49de2f1ef441.4301202215315676631268.jpg" alt="user" />
-                                    <div className="user-info">
-                                        <h5>Bastien Oswald</h5>
-                                        <small>2h ago</small>
-                                    </div>
-                                </div>
-                                <div className='distance'>25 km</div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <a href={"/details"}>
-                    <div className="card">
-                        <div className="card-header">
-                            <img src="https://www.belambra.fr/les-echappees/wp-inside/uploads/2019/12/perdu-randonnee-reflexes.jpg" alt="rover" />
-                        </div>
-                        <div className="card-body">
-                            <h4>
-                                Dimanche 25 Février
-                            </h4>
-                            <h3>
-                                Trail du vieux Lac
-                            </h3>
-                            <p>
-                                Course à pied en nature, sur sentiers et chemins de montagne. Dépassez vos limites et admirez la beauté de la nature.
-                            </p>
-                            <div className="flex">
-                                <div className="user">
-                                    <img src="https://img1.freepng.fr/20180714/ggq/kisspng-user-profile-computer-icons-login-clip-art-profile-picture-icon-5b49de2f1ef441.4301202215315676631268.jpg" alt="user" />
-                                    <div className="user-info">
-                                        <h5>Bastien Oswald</h5>
-                                        <small>2h ago</small>
-                                    </div>
-                                </div>
-                                <div className='distance'>25 km</div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <a href={"/details"}>
-                    <div className="card">
-                        <div className="card-header">
-                            <img src="https://www.belambra.fr/les-echappees/wp-inside/uploads/2019/12/perdu-randonnee-reflexes.jpg" alt="rover" />
-                        </div>
-                        <div className="card-body">
-                            <h4>
-                                Dimanche 25 Février
-                            </h4>
-                            <h3>
-                                Trail du vieux Lac
-                            </h3>
-                            <p>
-                                Course à pied en nature, sur sentiers et chemins de montagne. Dépassez vos limites et admirez la beauté de la nature.
-                            </p>
-                            <div className="flex">
-                                <div className="user">
-                                    <img src="https://img1.freepng.fr/20180714/ggq/kisspng-user-profile-computer-icons-login-clip-art-profile-picture-icon-5b49de2f1ef441.4301202215315676631268.jpg" alt="user" />
-                                    <div className="user-info">
-                                        <h5>Bastien Oswald</h5>
-                                        <small>2h ago</small>
-                                    </div>
-                                </div>
-                                <div className='distance'>25 km</div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <a href={"/details"}>
-                    <div className="card">
-                        <div className="card-header">
-                            <img src="https://www.belambra.fr/les-echappees/wp-inside/uploads/2019/12/perdu-randonnee-reflexes.jpg" alt="rover" />
-                        </div>
-                        <div className="card-body">
-                            <h4>
-                                Dimanche 25 Février
-                            </h4>
-                            <h3>
-                                Trail du vieux Lac
-                            </h3>
-                            <p>
-                                Course à pied en nature, sur sentiers et chemins de montagne. Dépassez vos limites et admirez la beauté de la nature.
-                            </p>
-                            <div className="flex">
-                                <div className="user">
-                                    <img src="https://img1.freepng.fr/20180714/ggq/kisspng-user-profile-computer-icons-login-clip-art-profile-picture-icon-5b49de2f1ef441.4301202215315676631268.jpg" alt="user" />
-                                    <div className="user-info">
-                                        <h5>Bastien Oswald</h5>
-                                        <small>2h ago</small>
-                                    </div>
-                                </div>
-                                <div className='distance'>25 km</div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <a href={"/details"}>
-                    <div className="card">
-                        <div className="card-header">
-                            <img src="https://www.belambra.fr/les-echappees/wp-inside/uploads/2019/12/perdu-randonnee-reflexes.jpg" alt="rover" />
-                        </div>
-                        <div className="card-body">
-                            <h4>
-                                Dimanche 25 Février
-                            </h4>
-                            <h3>
-                                Trail du vieux Lac
-                            </h3>
-                            <p>
-                                Course à pied en nature, sur sentiers et chemins de montagne. Dépassez vos limites et admirez la beauté de la nature.
-                            </p>
-                            <div className="flex">
-                                <div className="user">
-                                    <img src="https://img1.freepng.fr/20180714/ggq/kisspng-user-profile-computer-icons-login-clip-art-profile-picture-icon-5b49de2f1ef441.4301202215315676631268.jpg" alt="user" />
-                                    <div className="user-info">
-                                        <h5>Bastien Oswald</h5>
-                                        <small>2h ago</small>
-                                    </div>
-                                </div>
-                                <div className='distance'>25 km</div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/courses", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => setCourses(response))
+      .catch((error) => console.log(error));
+  }, []);
 
+  return (
+    <div className="list">
+      <div className="list-container">
+        {courses.map(course=>
+            (<a href={"/details"}>
+            <div className="card">
+              <div className="card-header">
+                <img
+                  src="https://www.belambra.fr/les-echappees/wp-inside/uploads/2019/12/perdu-randonnee-reflexes.jpg"
+                  alt="rover"
+                />
+              </div>
+              <div className="card-body">
+                <h4>{new Date(course.date).toLocaleDateString('Fr-fr')}</h4>
+                <h3>{course.localisation.nom}</h3>
+               
+                <div className="flex">
+                  <div className="distance">{course.distance} m</div>
+                </div>
+              </div>
             </div>
-        </div>
-    )
-}
+          </a>))}
 
-export default List
+    
+      </div>
+    </div>
+  );
+};
+
+export default List;
