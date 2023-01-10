9e1d9f7b1feb1ed21c89e27d9e896329923819d7
paul <paul.gubbiotti@gmail.com>
2023-01-10 08:59:42 +0100

    Add Header.tsx

--- ReactTrail/src/App.tsx
+++ ReactTrail/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
-import { Outlet } from "react-router-dom"
+import {Outlet} from "react-router-dom"
+import Header from "./Components/Header";
+import Footer from './Components/footer';
+
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    return (
+        <div className={"container mx-auto pt-28"}>
+            <Header backgroundImage="/home.webp"
+                    namePage="Reactive Trail"
+                    description="Bienvenue sur notre site de trail ! "
+                    description3="courses vous attendent ! Bonne visite !"
+                    nbCourses={"56"}
+            />
+
+            <p className={"pt-2"}>
+                Bienvenue sur notre site de trail ! Nous sommes passionnés de course à pied en nature et avons créé ce
+                site pour partager notre amour du trail avec vous. Vous trouverez ici tout ce dont vous avez besoin pour
+                découvrir et pratiquer ce sport fascinant : conseils de préparation, itinéraires de courses, tests de
+                matériel, etc. Que vous soyez débutant ou expérimenté, nous espérons que vous trouverez ici de quoi
+                satisfaire votre soif de découverte et de performance. Rejoignez notre communauté en ligne et partagez
+                vos expériences de trail avec nous !
+            </p>
+
+            <Footer></Footer>
+
+        </div>
+    )
 }
 
 export default App
