b34ce8d029b8838336787090e59a98546b84cbdb
tjamard <jamard.theo17@gmail.com>
2023-01-11 15:29:50 +0100

    debut du formulaire ajout d'utilisateur

--- ReactTrail/src/Components/FormCreateUser.tsx
+++ ReactTrail/src/Components/FormCreateUser.tsx
@@ -1,41 +1,71 @@
+// @ts-nocheck - may need to be at the start of file
 import React, {useState} from "react";
-import {Transition} from "@headlessui/react";
 
 function FormCreateUser() {
-    const [isOpen, setIsOpen] = useState(false);
+    const [inputs, setInputs] = useState({});
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+
+        fetch('http://127.0.0.1:8000/api/utilisateurs', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(inputs)
+        })
+            .then(response => response.json())
+            .then(response => console.log(JSON.stringify(response)))
+            .catch(error => console.log(error))
+    }
+
+    const handleChange = (event: any) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }))
+    }
+
     return (
         <div className="w-full lg:px-0 px-3">
-            <form className="bg-white  rounded">
+            <form onSubmit={handleSubmit} className="bg-white  rounded">
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                         Identifiant
                     </label>
                     <input
                         className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="username" type="text" placeholder="antoine_zebulon" required  minlength="3" maxlength="30"/>
+                        id="username" name={"pseudo"} type="text" placeholder="antoine_zebulon" required  minLength={3} maxLength={30}
+                        value={inputs.pseudo || ""}
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                         Email
                     </label>
                     <input
                         className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="username" type="email" placeholder="antoine.zebulon@reactive-trail.fr" required/>
+                        id="email" name={"email"} type="email" placeholder="antoine.zebulon@reactive-trail.fr" required
+                        value={inputs.email || ""}
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="plainPassword">
                         Mot de passe
                     </label>
                     <input
                         className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="password" type="password" placeholder="******************" required minlength="8" maxlength="255"/>
-
+                        id="plainPassword" name={"plainPassword"} type="password" placeholder="******************" required minLength={8} maxLength={255}
+                        value={inputs.plainPassword || ""}
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className="flex items-center justify-between">
-                    <input className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Connexion"/>
+                    <input className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer" type="submit" value="Connexion"/>
                 </div>
             </form>
-
         </div>
     );
 }
