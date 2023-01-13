ecfb36bd9d3569ae3530cf2fbdc70834037124ba
TDpy <toto.dupuy6558@gmail.com>
2023-01-13 09:13:35 +0100

    fix carte avec les nouvelles appelations pour les ping sur la carte

---
+++ ReactTrail/src/Pages/listEvenements/Marker_.tsx
@@ -0,0 +1,34 @@
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+
+export default function Marker_({evenements}:any) {
+  
+  
+/*const MyMarkers = ({ data }:any) => {
+  return data.map((datas:any, index:any) => (
+    <Marker
+      key={index}
+      position={datas}
+    >
+    </Marker>
+  ));
+}*/
+
+  return (
+    <MapContainer
+      className="map"
+      center={[46.71109, 1.7191036]}
+      zoom={5}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {
+        evenements.map((evenement: object) => <Marker key={evenement.id} position={[evenement.localisation.lat, evenement.localisation.lng]}></Marker>)
+      }
+
+    </MapContainer>
+  )
+}
